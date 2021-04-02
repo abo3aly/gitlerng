61c0cd95c6806c64a1b96b7513ccbb6552ac2579
meshaal <aboremas1516@gmail.com>
2021-04-03 01:06:24 +0300

    Add project files.

---
+++ gitlerng/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gitlerng
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string directory = @"C:\Program Files\Git\git-bash.exe"; // directory of the git repository
+
+            
+            using (System.Management.Automation.PowerShell powershell = System.Management.Automation.PowerShell.Create())
+            {
+                // this changes from the user folder that PowerShell starts up with to your git repository
+                powershell.AddScript($"cd {directory}");
+                powershell.AddScript(@"git init");
+                Collection<System.Management.Automation.PSObject> results = powershell.Invoke();
+            }
+        }
+
+       
+    }
+}
